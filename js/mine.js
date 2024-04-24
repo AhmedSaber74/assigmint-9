dc47c2d20ef1f17b8228cd05b042cce6ab005b71
AhmedSaber <as5113336@gmail.com>
2024-04-25 01:38:35 +0200

    first commit

--- js/mine.js
+++ js/mine.js
@@ -1,109 +1,149 @@
 
-var pNameInput = document.getElementById("NameId") ; 
-var pUrlInput = document.getElementById("URLId") ; 
-var cartoona=[] ;
+// var pNameInput = document.getElementById("NameId") ; 
+// var pUrlInput = document.getElementById("URLId") ; 
+// var cartoona=[] ;
 
-var currentIndex ; 
+// var currentIndex ; 
 
-if(  localStorage.getItem("localProducts") == null )
-{
-    cartoona = [] ;
-}
-else
-{
-      cartoona =  JSON.parse(localStorage.getItem("localProducts")) ;
+// if(  localStorage.getItem("localProducts") == null )
+// {
+//     cartoona = [] ;
+// }
+// else
+// {
+//       cartoona =  JSON.parse(localStorage.getItem("localProducts")) ;
 
-      displayInfo()
-}
+//       displayInfo()
+// }
 
-var nameRegex = /^\w{3,}(\s+\w+)*$/;
+// var nameRegex = /^\w{3,}(\s+\w+)*$/;
 
-var urlRegex = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
+// var urlRegex = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
 
-JSON.parse(localStorage.getItem("localProducts"))
-function addInfo()
-{
-    if( cartoona.indexOf(pNameInput.value===-1) && nameRegex.test(pNameInput.value)&& urlRegex.test(pUrlInput.value) )
-    {
-        var oneProduct = {
+// JSON.parse(localStorage.getItem("localProducts"))
+// function addInfo()
+// {
+//     if( cartoona.indexOf(pNameInput.value===-1) && nameRegex.test(pNameInput.value)&& urlRegex.test(pUrlInput.value) )
+//     {
+//         var oneProduct = {
 
-            pName : pNameInput.value ,
-            pUrl : pUrlInput.value ,
-        }
+//             pName : pNameInput.value ,
+//             pUrl : pUrlInput.value ,
+//         }
 
-            cartoona.push(oneProduct);
+//             cartoona.push(oneProduct);
          
-            localStorage.setItem("localProducts" ,  JSON.stringify(cartoona) )
+//             localStorage.setItem("localProducts" ,  JSON.stringify(cartoona) )
         
-            displayInfo()
+//             displayInfo()
         
-            clearInputs()
+//             clearInputs()
            
         
         
     
        
     
-    }
-    else
-    {
-        swal(  "Site Name or Url is not valid, Please follow the rules below :", `Site name must contain at least 3 characters
-        Site URL must be a valid one
-        `);
-    }
+//     }
+//     else
+//     {
+//         swal(  "Site Name or Url is not valid, Please follow the rules below :", `Site name must contain at least 3 characters
+//         Site URL must be a valid one
+//         `);
+//     }
 
 
 
    
-}
+// }
 
-function clearInputs()
-{
-    pNameInput.value = null;
-    pUrlInput.value = null;
-}
+// function clearInputs()
+// {
+//     pNameInput.value = null;
+//     pUrlInput.value = null;
+// }
 
 
-function displayInfo()
-{
+// function displayInfo()
+// {
 
-    var hasalah = ``;
+//     var hasalah = ``;
 
-    for(var  i =1 ; i < cartoona.length ; i++)
-    {
-      hasalah +=  ` <tr>
-            <td>${i}</td>
-            <td>${cartoona[i].pName}</td>
-            <td>
-            <a class="btn btn-outline-warning" href="${cartoona[i].pUrl}" target="_blank"> Visit</a>            </td>
-            <td>
-             <button onclick="deleteInfo(${i})" class="btn btn-outline-danger">Delete</button>
-            </td>
+//     for(var  i =1 ; i < cartoona.length ; i++)
+//     {
+//       hasalah +=  ` <tr>
+//             <td>${i}</td>
+//             <td>${cartoona[i].pName}</td>
+//             <td>
+//             <a class="btn btn-outline-warning" href="${cartoona[i].pUrl}" target="_blank"> Visit</a>            </td>
+//             <td>
+//              <button onclick="deleteInfo(${i})" class="btn btn-outline-danger">Delete</button>
+//             </td>
         
-      </tr>`
-    }
+//       </tr>`
+//     }
 
-    document.getElementById("tBody").innerHTML = hasalah;
+//     document.getElementById("tBody").innerHTML = hasalah;
     
+// }
+
+
+// function deleteInfo(pIndex)
+// {
+//     cartoona.splice( pIndex , 1 ) 
+
+//     localStorage.setItem("localProducts" , JSON.stringify(  cartoona ))
+//     displayInfo() 
+// }
+
+
+
+
+
+
+//     //  document.getElementById("addBtn").style.display = "block";
+//     //  document.getElementById("updateBtn").style.display = "none";
+
+
+
+// document.addEventListener("mousemove" , function(eInfo){
+
+//     document.querySelector("img").style.top = eInfo.clientY +"px"
+//     document.querySelector("img").style.left = eInfo.clientX +"px"
+
+
+
+// // })
+
+// var allImgs = Array.from(document.querySelectorAll("img"))
+
+
+// for (let i = 0; i < allImgs.length; i++) {
+   
+
+//     allImgs[i].addEventListener("click" , function(eInfo){
+
+//         var imgSrc = eInfo.target.src;
+
+//         document.querySelector(".mainDiv").style.backgroundImage=`url(${imgSrc})`
+
+//     })
+
+    
+// }
+
+
+var allImgs = Array.from(document.querySelectorAll("img"))
+
+var layerParent = document.querySelector(".layerParent")
+for (var i = 0; i < allImgs.length; i++) {
+
+
+
+    allImgs[i].addEventListener("click" , function(eInfo){
+
+        layerParent.classList.replace("d-none" , "d-flex")
+        
+    
+        })      
 }
-
-
-function deleteInfo(pIndex)
-{
-    cartoona.splice( pIndex , 1 ) 
-
-    localStorage.setItem("localProducts" , JSON.stringify(  cartoona ))
-    displayInfo() 
-}
-
-
-
-
-
-
-    //  document.getElementById("addBtn").style.display = "block";
-    //  document.getElementById("updateBtn").style.display = "none";
-
-
-
-
